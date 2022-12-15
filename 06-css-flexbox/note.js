207e9cc8c5ad240315c632c6e40f1334ce830997
ADDFa <fratamaadha18@gmail.com>
2022-12-15 16:26:09 +0700

    flexbox

---
+++ 06-css-flexbox/note.js
@@ -0,0 +1,31 @@
+// flexbox -> model layout 1 dimensi
+
+
+// Property
+// (container / pembungkus / parent)
+
+// ---------------
+// display: flex;
+// flex-direction: row | row-reverse | column | column-reverse;
+// flex-wrap: nowrap | wrap | wrap reverse;
+// justify-content: flex-start | flex-end | center | space-between | space-around | space-evenly;
+// align-items: flex-start | flex-end | center | strecth | baseline;
+// align-content: flex-start | flex-end | center | strecth | baseline;
+// column-gap | row-gap | gap : [angka];
+// ---------------
+
+
+
+
+
+
+// property
+// Items (child)
+
+// ---------------
+// order: [angka];
+// flex-grow: [angka];
+// flex-shrink: [angka];
+// flex-basis: [ukuran];
+// align-self: auto | flex-start | flex-end | center | baseline | strecth;
+// ---------------
